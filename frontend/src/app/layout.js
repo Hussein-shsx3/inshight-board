0cb3b78784abf3e601cd07d1557c5b6ad43aa143
Hussein Mohammed <husseinshsx3@gmail.com>
2025-11-14 11:24:44 +0200

    Update layout.js
    Add top margin to push below header

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -25,6 +25,7 @@ export default function RootLayout({ children }) {
               style: {
                 background: "#363636",
                 color: "#fff",
+                marginTop: "80px", // Add top margin to push below header
               },
               success: {
                 duration: 3000,
